1043dd10665f1f2485db6c9a12067e858e193f73
tyayers <tyler.ayers@gmail.com>
2022-03-21 09:31:16 +0100

    updated with envoy 1.18 config

---
+++ start_local.sh
@@ -0,0 +1,1 @@
+envoy -c config_1.18.yaml
--- test.sh
+++ test.sh
@@ -1,4 +1,10 @@
 
-curl -H "Authorization: Bearer $TOKEN" http://35.195.226.255:10000
+#curl -H "Authorization: Bearer $TOKEN" http://35.195.226.255:10000
 
-wrk -H "Authorization: Bearer $TOKEN" -t12 -c100 -d180s -R2000 http://35.195.226.255:10000
+#wrk -H "Authorization: Bearer $TOKEN" -t12 -c100 -d180s -R2000 http://35.195.226.255:10000
+
+# First without envoy (local test)
+wrk -t40 -c400 -d30s -R4000 http://localhost:8080
+
+# Now with envoy
+wrk -H "Authorization: Bearer $TOKEN" -t40 -c400 -d30s -R4000 http://localhost:10000
